e9893be290ebe1b87be9aea1ea0fc07060fda1a8
EduardoMijangos <lalomijafru126@gmail.com>
2023-08-07 09:11:55 -0600

    Register alumnos funcionando y corregido

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,7 +9,7 @@ export class AuthService {
   private loginUrl = 'http://localhost:3000/api/escuela/login';
   private registerUrl = 'http://localhost:3000/api/escuela/register';
   private registerpadresUrl = 'http://localhost:3000/api/padres/registrarPadre';
-  private registerAlumnosUrl = 'http://localhost:3000/api/padres/registrarPadre';
+  private registerAlumnosUrl = 'http://localhost:3000/api/hijos/registrarHijo';
   private currentUserMatricula: string | null = null;
 
 
@@ -35,8 +35,8 @@ export class AuthService {
     return this.http.post(this.registerpadresUrl, body);
   }
 
-  registerAlumnos(nombre: string, apellidos: string, grupo: string, matricula: string, foto: string, observaciones:string, padreid:number): Observable<any> {
-    const body = {nombre:nombre,apellidos:apellidos,grupo:grupo,matricula:matricula, foto:foto, observaciones:observaciones, padreid: padreid };
+  registerAlumnos(nombre: string, apellidos: string, grupo: string, matricula: string, foto: string, observaciones: string, padreID: number): Observable<any> {
+    const body = { nombre: nombre, apellidos: apellidos, grupo: grupo, matricula: matricula, foto: foto, observaciones: observaciones, padreID: padreID };
     return this.http.post(this.registerAlumnosUrl, body);
   }
 
